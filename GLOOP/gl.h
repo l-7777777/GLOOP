7f8980ef0cc435605f1110ea48cd9c1302a22cb4
l-7777777 <lysander.mealy@gmail.com>
2021-04-29 15:35:27 -0400

    Initial commit

---
+++ GLOOP/gl.h
@@ -0,0 +1,403 @@
+﻿#pragma once
+#include <cstring>
+
+#include "gl3w/include/GL/gl3w.h"
+#include "gl3w/include/GL/glcorearb.h"
+
+class GL
+{
+public:
+    template<typename T>
+    class Vector4
+    {
+    public:
+        // x y z w
+        T x;
+        T y;
+        T z;
+        T w;
+
+        // r g b a
+        T r;
+        T g;
+        T b;
+        T a;
+
+        Vector4 *operator=(T arr[4])
+        {
+            one   = arr[0];
+            two   = arr[1];
+            three = arr[2];
+            four  = arr[3];
+            reSyncPrivate();
+            return this;
+        }
+
+        Vector4 *operator+=(Vector4<T> other)
+        {
+            one   += other.one;
+            two   += other.two;
+            three += other.three;
+            four  += other.four;
+            reSyncPrivate();
+            return this;
+        }
+
+        Vector4 *operator-=(Vector4<T> other)
+        {
+            one   -= other.one;
+            two   -= other.two;
+            three -= other.three;
+            four  -= other.four;
+            reSyncPrivate();
+            return this;
+        }
+
+        Vector4 *operator*=(Vector4<T> other)
+        {
+            one   *= other.one;
+            two   *= other.two;
+            three *= other.three;
+            four  *= other.four;
+            reSyncPrivate();
+            return this;
+        }
+
+        Vector4 *operator/=(Vector4<T> other)
+        {
+            one   /= other.one;
+            two   /= other.two;
+            three /= other.three;
+            four  /= other.four;
+            reSyncPrivate();
+            return this;
+        }
+
+        Vector4 operator+(Vector4<T> other)
+        {
+            Vector4 copy;
+            copy = this;
+            copy += other;
+            return copy;
+        }
+
+        Vector4 operator-(Vector4<T> other)
+        {
+            Vector4 copy;
+            copy = this;
+            copy -= other;
+            return copy;
+        }
+
+        Vector4 operator*(Vector4<T> other)
+        {
+            Vector4 copy;
+            copy = this;
+            copy *= other;
+            return copy;
+        }
+
+        Vector4 operator/(Vector4<T> other)
+        {
+            Vector4 copy;
+            copy = this;
+            copy /= other;
+            return copy;
+        }
+
+        void reSyncXYZW()
+        {
+            one   = x;
+            two   = y;
+            three = z;
+            four  = w;
+        }
+
+        void reSyncRGBA()
+        {
+            one   = r;
+            two   = g;
+            three = b;
+            four  = a;
+        }
+
+    private:
+        void reSyncPrivate()
+        {
+            x = one;
+            y = two;
+            z = three;
+            w = four;
+            
+            r = one;
+            g = two;
+            b = three;
+            a = four;
+        }
+        
+        T one;
+        T two;
+        T three;
+        T four;
+    };
+
+    template<typename T>
+    class Vector3
+    {
+        public:
+        // x y z
+        T x;
+        T y;
+        T z;
+
+        // r g b
+        T r;
+        T g;
+        T b;
+
+        Vector3 *operator=(T arr[3])
+        {
+            one   = arr[0];
+            two   = arr[1];
+            three = arr[2];
+            reSyncPrivate();
+            return this;
+        }
+
+        Vector3 *operator+=(Vector3<T> other)
+        {
+            one   += other.one;
+            two   += other.two;
+            three += other.three;
+            reSyncPrivate();
+            return this;
+        }
+
+        Vector3 *operator-=(Vector3<T> other)
+        {
+            one   -= other.one;
+            two   -= other.two;
+            three -= other.three;
+            reSyncPrivate();
+            return this;
+        }
+
+        Vector3 *operator*=(Vector3<T> other)
+        {
+            one   *= other.one;
+            two   *= other.two;
+            three *= other.three;
+            reSyncPrivate();
+            return this;
+        }
+
+        Vector3 *operator/=(Vector3<T> other)
+        {
+            one   /= other.one;
+            two   /= other.two;
+            three /= other.three;
+            reSyncPrivate();
+            return this;
+        }
+
+        Vector3 operator+(Vector3<T> other)
+        {
+            Vector3 copy;
+            copy = this;
+            copy += other;
+            return copy;
+        }
+
+        Vector3 operator-(Vector3<T> other)
+        {
+            Vector3 copy;
+            copy = this;
+            copy -= other;
+            return copy;
+        }
+
+        Vector3 operator*(Vector3<T> other)
+        {
+            Vector3 copy;
+            copy = this;
+            copy *= other;
+            return copy;
+        }
+
+        Vector3 operator/(Vector3<T> other)
+        {
+            Vector3 copy;
+            copy = this;
+            copy /= other;
+            return copy;
+        }
+
+        void reSyncXYZ()
+        {
+            one   = x;
+            two   = y;
+            three = z;
+        }
+
+        void reSyncRGB()
+        {
+            one   = r;
+            two   = g;
+            three = b;
+        }
+
+    private:
+        void reSyncPrivate()
+        {
+            x = one;
+            y = two;
+            z = three;
+            
+            r = one;
+            g = two;
+            b = three;
+        }
+        
+        T one;
+        T two;
+        T three;
+    };
+
+    template<typename T>
+    class Vector2
+    {
+        public:
+        // x y
+        T x;
+        T y;
+
+        // r g
+        T s;
+        T t;
+
+        Vector2 *operator=(T arr[2])
+        {
+            one = arr[0];
+            two = arr[1];
+            reSyncPrivate();
+            return this;
+        }
+
+        Vector2 *operator+=(Vector2<T> other)
+        {
+            one   += other.one;
+            two   += other.two;
+            reSyncPrivate();
+            return this;
+        }
+
+        Vector2 *operator-=(Vector2<T> other)
+        {
+            one   -= other.one;
+            two   -= other.two;
+            reSyncPrivate();
+            return this;
+        }
+
+        Vector2 *operator*=(Vector2<T> other)
+        {
+            one   *= other.one;
+            two   *= other.two;
+            reSyncPrivate();
+            return this;
+        }
+
+        Vector2 *operator/=(Vector2<T> other)
+        {
+            one   /= other.one;
+            two   /= other.two;
+            reSyncPrivate();
+            return this;
+        }
+
+        Vector2 operator+(Vector2<T> other)
+        {
+            Vector2 copy;
+            copy = this;
+            copy += other;
+            return copy;
+        }
+
+        Vector2 operator-(Vector2<T> other)
+        {
+            Vector2 copy;
+            copy = this;
+            copy -= other;
+            return copy;
+        }
+
+        Vector2 operator*(Vector2<T> other)
+        {
+            Vector2 copy;
+            copy = this;
+            copy *= other;
+            return copy;
+        }
+
+        Vector2 operator/(Vector2<T> other)
+        {
+            Vector2 copy;
+            copy = this;
+            copy /= other;
+            return copy;
+        }
+
+        void reSyncXYZW()
+        {
+            one   = x;
+            two   = y;
+        }
+
+        void reSyncRGBA()
+        {
+            one   = s;
+            two   = t;
+        }
+
+    private:
+        void reSyncPrivate()
+        {
+            x = one;
+            y = two;
+            
+            s = one;
+            t = two;
+        }
+        
+        T one;
+        T two;
+    };
+
+    static int init()
+    {
+        if (gl3wInit())
+        {
+            return -1;
+        }
+        return 0;
+    }
+
+    // GL wrapper functions
+    // glClear
+    static void clear(int mask)
+    {
+        glClear(mask);
+    }
+    // glBegin
+    static void begin(int mode)
+    {
+        ((void(*)(int))gl3wGetProcAddress("glBegin"))(mode);
+    }
+    
+    // GL wrapper constants
+    // glClear constants
+    const int colorBufferBit   = GL_COLOR_BUFFER_BIT;
+    const int depthBufferBit   = GL_DEPTH_BUFFER_BIT;
+    const int accumBufferBit   = 0x00000200;
+    const int stencilBufferBit = GL_STENCIL_BUFFER_BIT;
+};
---
+++ readme.md
@@ -0,0 +1,4 @@
+﻿# GLOOP: An Object-Oriented wrapper on OpenGL
+GLOOP is essentially a wrapper around OpenGL. It includes OpenGL itself, GLFW, and GL3W.
+
+###### More readme coming soon...
